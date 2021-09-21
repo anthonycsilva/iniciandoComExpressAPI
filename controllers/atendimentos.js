761ca9ca958ac64226cdb0a63f14c605bce78565
Your Name <you@example.com>
2021-09-21 07:09:59 -0300

    subindo o projeto no github

---
+++ controllers/atendimentos.js
@@ -0,0 +1,14 @@
+module.exports = app => {
+    app.get('/atendimentos', (req, res) => {
+        console.log('Pedido Enviado');
+        res.send('Você está na rota de atendimentos e está realizando um GET')})
+
+    app.post('/atendimentos', (req, res) => {
+        console.log('Pedido Recebido pelo servidor')
+
+        const dado = req.body
+        res.send('Post atendimento')
+    })
+     
+     
+}
